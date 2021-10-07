e4152511c0cfd6543edba237fa0229a1d2718f6e
VitorB2002 <vitorb.santos2002@gmail.com>
2021-10-07 16:28:19 -0300

    Teste unitário de Crud em cliente e otimização do teste produto

---
+++ TP5/src/teste/TesteCrudCliente.java
@@ -0,0 +1,60 @@
+package teste;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import controle.*;
+
+class TesteCrudCliente {
+	
+	private ControleDados dados = new ControleDados();
+	String[] s = new String[5];
+	String[] s2 = new String[5];
+	private int aux;
+	private int posicao;
+	
+	
+	/*
+	 * O metodo editarCadastrarCliente realiza o cadastro e a edição do mesmo
+	 * Para isso o mesmo valida se os dados recebidos são validos
+	 * Ou seja:
+	 * Nenhum campo deve estar vazio 
+	 * Os campos cpf e telefone apenas recebem números
+	 * */
+	
+	@Test
+	void testCadastroCliente() {
+		
+		//Aux informa para o metodo se queremos cadastrar ou editar, aux = 1 (cadastra), aux!= 1 (edita)
+		aux = 1;
+		
+		String[] s = {"Vitor Borges", "05395016201", "Q.18 C.08", "vitorb.santos@gmail.com", "61992151043"};
+		
+		assertTrue(dados.editarCadastrarCliente(s, aux, 0));
+		
+		String[] s2 = {"Vitor Borges", "cpf", "Q.18 C.08", "vitorb.santos@gmail.com", "telefone"};
+		
+		assertFalse(dados.editarCadastrarCliente(s2, aux, 0));
+		
+	}
+	
+	//O metodo de edição realiza as mesmas validações logo o String[] s2 não pode editar um cliente
+	@Test
+	void testEdicaoCliente() {
+		
+		aux = 2;
+		posicao = 0;//O cliente cadastrado no index 0 é o Cliente0
+		
+		assertFalse("Vitor Borges".equals(dados.getDados().getClientes().get(posicao).getNome()));
+		
+		String[] s = {"Vitor Borges", "05395016201", "Q.18 C.08", "vitorb.santos@gmail.com", "61992151043"};
+		
+		assertTrue(dados.editarCadastrarCliente(s, aux, posicao));
+		assertTrue("Vitor Borges".equals(dados.getDados().getClientes().get(posicao).getNome()));
+		
+		String[] s2 = {"Vitor Borges", "cpf", "Q.18 C.08", "vitorb.santos@gmail.com", "telefone"};
+		
+		assertFalse(dados.editarCadastrarCliente(s2, aux, posicao));
+	}
+
+}
--- TP5/src/teste/TesteCrudProduto.java
+++ TP5/src/teste/TesteCrudProduto.java
@@ -1,6 +1,7 @@
 package teste;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import controle.*;
 
@@ -46,13 +47,21 @@ class TesteCrudProduto {
 	//As exigências para edição são iguais as exigências para o cadastro
 	@Test
 	void testEdicaoProduto() {
+		
 		//O aux exerce a mesma função aqui porém agora Capa = 2, Carregador = 4, Pelicula = 6 e Fone = 8
 		aux = 2;
+		
 		//A posição aqui é importante, estaremos sobrescrevendo o modelo Capa0 posicao = 0
 		posicao = 0;
-		//Teste para Capas
+		
+		//Verificando que Capinha não ocupa a posição 0 do ArrayList
+		assertFalse("Capinha".equals(estoque.getEstoque().getCapas().get(posicao).getModelo()));
+		
 		String[] s = {"Capinha", "Anti-Impacto","25.6","ASD","Silicone","A8","0.5","Vermelho"};
+		
+		//Comprovando edição
 		assertTrue(estoque.editarCadastrarProduto(s, 2, 0));
+		assertTrue("Capinha".equals(estoque.getEstoque().getCapas().get(posicao).getModelo()));
 		
 		String[] s2 = {"Capinha", "","25.6","ASD","Silicone","A8","0.5","Vermelho"};
 		assertFalse(estoque.editarCadastrarProduto(s2, 2, 0));
@@ -60,14 +69,18 @@ class TesteCrudProduto {
 		String[] s3 = {"","","","","","","",""};
 		assertFalse(estoque.editarCadastrarProduto(s3, 2, 0));
 		
+		
+		
 	}
 	
 	//O metodo deletar não retorna boolean como os outros, logo o teste é um pouco diferente
 	@Test
 	void testDeletaProduto() {
+		
 		//O aux exerce a mesma função aqui porém agora Capa = 1, Carregador = 2, Pelicula = 3 e Fone = 4
 		aux = 1;
 		posicao = 0;
+		
 		//Obtendo o modelo do produto, o index 0 é equivalente ao modelo pré cadastrado = Capa0
 		s[0] = estoque.getEstoque().getCapas().get(0).getModelo();
 		
@@ -80,6 +93,7 @@ class TesteCrudProduto {
 		 * Logo na comparação abaixo estaremos comparando Capa0 com Capa1
 		 * o que retornará false
 		 * */
+		
 		sucesso = s[0].equals(estoque.getEstoque().getCapas().get(0).getModelo());
 		
 		assertFalse(sucesso);
